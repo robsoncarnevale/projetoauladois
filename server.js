7589f4a22408f78ff205de56ed7d68a7ec6e0eaa
Robson Carnevale <rccimini@gmail.com>
2020-09-30 16:43:43 -0300

    Update server.js
    acrescentei rota olá

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req,res)=> {
     res.status(200).send({message:"Olá Mundo estou funfando"})
 })
 
+app.get('/ola', (req,res)=> {
+    res.status(200).send({message:"Adiciona a rota olá"})
+})
+
 app.listen(3001, () => {
     console.log("API rodando na porta 3001");
 })
